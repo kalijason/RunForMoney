f9bad97609991eedf3ff260d9525a66367c1d719
kalijason <kalijason@gmail.com>
2012-08-08 03:23:11 +0800

    Add lost class

---
+++ src/runner/event/CommandEvent.java
@@ -0,0 +1,33 @@
+package runner.event;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+import runner.game.GameController;
+
+public class CommandEvent implements Listener {
+
+	GameController gameController;
+
+	public CommandEvent(GameController gameController) {
+		this.gameController = gameController;
+	}
+
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onCommand(
+PlayerCommandPreprocessEvent event) {
+		Player player = event.getPlayer();
+		if (player != null
+				&& player instanceof Player
+				&& ((gameController.getRunner(player) != null) || gameController
+						.getHunter(player) != null)) {
+			event.setCancelled(true);
+			player.sendMessage(ChatColor.BLUE + "遊戲中不可以使用指令");
+		}
+
+	}
+}
---
+++ src/runner/game/PrizeDispatcher.java
@@ -0,0 +1,56 @@
+package runner.game;
+
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import runner.RunForMoney;
+import runner.util.PlayerUtil;
+
+public class PrizeDispatcher {
+	private RunForMoney runForMoney;
+
+	public PrizeDispatcher(RunForMoney runForMoney) {
+		this.runForMoney = runForMoney;
+	}
+
+	public void dispatchToHunters(List<RFMPlayer> hunterList) {
+		// 抓到玩家大於一人則有一個獎品，再來每抓三人就有一個獎品
+		for (RFMPlayer p : hunterList) {
+			if (p.getKills() > 0) {
+				givePrize(p, 1, (p.getKills() / 3) + 1);
+			}
+
+		}
+	}
+
+	public void dispatchToRunners(List<RFMPlayer> runnerList) {
+		for (RFMPlayer p : runnerList) {
+			if (p.isAlive()) {
+				givePrize(p, 1, 2);
+			}
+		}
+	}
+
+	private Material getRandomPrizeByLevel(int level) {
+		final Material[] priceList = { Material.DIAMOND, Material.GLOWSTONE,
+				Material.SLIME_BALL };
+		// get random between
+		int r = (int) (Math.random() * priceList.length);
+
+		return priceList[r];
+	}
+
+	private void givePrize(RFMPlayer rfmPlayer, int level, int number) {
+		Player player = runForMoney.getServer().getPlayer(rfmPlayer.getName());
+		if (player != null) {
+
+			for (int i = 0; i < number; i++) {
+				Material type = getRandomPrizeByLevel(level);
+				PlayerUtil.giveItem(player, type, 1);
+			}
+
+		}
+	}
+
+}
